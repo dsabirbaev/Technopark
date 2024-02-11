5e4cd6e9580269780b91d379ac3d7e196807094d
dsabirbaev <davran9559@gmail.com>
2024-02-12 03:11:51 +0500

    done modal window

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@
 const menu = document.querySelector(".nav__btn");
 const menuList = document.querySelector(".nav__list");
 const menuClose = document.querySelector(".menu-close");
-
+const openModal = document.querySelector(".payment-top__btn");
+const modalWindow = document.querySelector(".payment__modal");
+const closeModal = document.querySelector(".close-modal");
 menu.addEventListener("click", () => {
 
     if (menuList.style.display === "block") { 
@@ -19,3 +21,13 @@ menu.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
     menuList.style.display = "none";
 })
+
+openModal.addEventListener("click", () => {
+    modalWindow.style.display = "block";
+})
+
+closeModal.addEventListener("click", () => {
+    modalWindow.style.display = "none";
+})
+
+
